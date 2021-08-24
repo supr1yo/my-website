7b102fac616684e87d29dac7777ef82cc6013f17
supr1yo <supriyo.developer@gmail.com>
2021-08-24 14:35:36 +0530

    delete

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,16 +3,25 @@ import Fade from "react-reveal";
 
 const Home = () => {
   return (
-    <div>
-      <div className="text-center">
+    <div className="bg-blue-300  h-screen">
+      <div className="bg-gray-800">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <h1>asd</h1>
+        </div>
+
+      </div>
+      {/* <div className="text-center">
         <Fade bottom>
           <div>
             <h1 class="text-4xl font-bold text-purple-600">
               Supriyo's Website
             </h1>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Button
+            </button>
           </div>
         </Fade>
-      </div>
+      </div> */}
     </div>
   );
 };
